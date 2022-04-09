46800787e0f06d01362d74548159ba1e6b74ef8e
Kacper Prawda <skipzyeah@gmail.com>
2022-04-09 04:58:23 +0200

    asd

--- src/components/userApp/UserApp.jsx
+++ src/components/userApp/UserApp.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import styles from './UserApp.module.scss'   
 
 const UserApp = () => {
     return (
-        <div>
-            <h1>User App</h1>
+        <div className={styles.bg}>
+            <div className={styles.smallSqContainer}>
+                <div className={styles.smallSq}>VIDEO</div>
+                <div className={styles.smallSq}>TEXT</div>
+                <div className={styles.smallSq}>URL</div>
+                <div className={styles.smallSq}>PICTURE</div>
+            </div>
+            <div className={styles.mainSquare}>
+            
+            <textarea className={styles.textArea} name="comment" form="usrform">COPY ARTICLE HERE...</textarea>
+            <div className={styles.containerButtons}>
+            <button className={styles.btnSecondary}>BACK</button>
+            <button className={styles.btnPrimary}>CHECK</button>
+            </div>
+            </div>
         </div>
     );
 };
+
+
 
 export default UserApp;
